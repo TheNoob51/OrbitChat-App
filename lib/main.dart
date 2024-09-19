46b09d47375ebae9c6f3cfbf679803ce4f92f9bf
Gyan Vardhan <115806215+TheNoob51@users.noreply.github.com>
2024-09-20 00:48:17 +0530

    make it save the data (like if you close the app, it will still make you login, until you log out(use of stream))

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:devfolio_genai/HomePage/homepage.dart';
 import 'package:devfolio_genai/firebase_options.dart';
 import 'package:devfolio_genai/Login%20Page/loginpage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -18,13 +20,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const LoginScreen(),
-    );
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              } else if (snapshot.hasData) {
+                return const Homepage();
+              } else if (snapshot.hasError) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "An error occurred",
+                      style: TextStyle(
+                          color: Colors.red, fontWeight: FontWeight.bold),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        Firebase.initializeApp(
+                          options: DefaultFirebaseOptions.currentPlatform,
+                        );
+                      },
+                      child: const Text("Retry"),
+                    )
+                  ],
+                );
+              } else {
+                return const LoginScreen();
+              }
+            }));
   }
 }
